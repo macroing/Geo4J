3e45dfcec976389b43be1c51eea1d9f7168c5002
macroing <joergen.lundgren@macroing.org>
2023-01-31 08:53:34 +0100

    Update the Shape Cylinder and Shape Disk APIs.

--- src/main/java/org/macroing/geo4j/shape/cylinder/Cylinder3FReader.java
+++ src/main/java/org/macroing/geo4j/shape/cylinder/Cylinder3FReader.java
@@ -21,7 +21,6 @@ package org.macroing.geo4j.shape.cylinder;
 import java.io.DataInput;
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.lang.reflect.Field;//TODO: Add Unit Tests!
 import java.util.Objects;
 
 import org.macroing.geo4j.common.AngleF;
@@ -61,7 +60,6 @@ public final class Cylinder3FReader implements Shape3FReader {
 	 * @throws NullPointerException thrown if, and only if, {@code dataInput} is {@code null}
 	 * @throws UncheckedIOException thrown if, and only if, an I/O error occurs
 	 */
-//	TODO: Add Unit Tests!
 	@Override
 	public Cylinder3F read(final DataInput dataInput) {
 		try {
@@ -91,7 +89,6 @@ public final class Cylinder3FReader implements Shape3FReader {
 	 * @throws NullPointerException thrown if, and only if, {@code dataInput} is {@code null}
 	 * @throws UncheckedIOException thrown if, and only if, an I/O error occurs
 	 */
-//	TODO: Add Unit Tests!
 	@Override
 	public Cylinder3F read(final DataInput dataInput, final int id) {
 		Objects.requireNonNull(dataInput, "dataInput == null");
@@ -111,7 +108,6 @@ public final class Cylinder3FReader implements Shape3FReader {
 	 * @param id the ID to check
 	 * @return {@code true} if, and only if, {@code id == Cylinder3F.ID}, {@code false} otherwise
 	 */
-//	TODO: Add Unit Tests!
 	@Override
 	public boolean isSupported(final int id) {
 		return id == Cylinder3F.ID;
